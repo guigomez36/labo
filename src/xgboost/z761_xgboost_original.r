83c9b1651ab494c372164df5e88572696c81c056
guigomez36 <guillermo.ggg.gomez@gmail.com>
2022-09-27 22:34:38 -0300

    iteracion 25. param= list( objective=       "binary:logistic",                                    max_depth=           13,                                    min_child_weight=    6,                                    eta=                 0.0102,                                    colsample_bytree=    0.6376,                                    gamma=               0.0,                                    alpha=               0.0,                                    lambda=              0.0,                                    subsample=           1.0,                                    scale_pos_weight=    1.0                                    ),

--- src/xgboost/z761_xgboost_original.r
+++ src/xgboost/z761_xgboost_original.r
@@ -8,10 +8,10 @@ require("data.table")
 require("xgboost")
 
 #Aqui se debe poner la carpeta de la computadora local
-setwd("~/buckets/b1/")   #Establezco el Working Directory
+setwd("C:\\DataMining_Economia_Finanzas\\")   #Establezco el Working Directory
 
 #cargo el dataset donde voy a entrenar
-dataset  <- fread("./datasets/competencia2_2022.csv.gz", stringsAsFactors= TRUE)
+dataset  <- fread("C:\\DataMining_Economia_Finanzas\\datasets\\competencia2_2022.csv.gz", stringsAsFactors= TRUE)
 
 
 #paso la clase a binaria que tome valores {0,1}  enteros
@@ -28,10 +28,10 @@ dtrain  <- xgb.DMatrix( data= data.matrix(  dataset[ foto_mes==202103 , campos_b
 #genero el modelo con los parametros por default
 modelo  <- xgb.train( data= dtrain,
                       param= list( objective=       "binary:logistic",
-                                   max_depth=           6,
-                                   min_child_weight=    1,
-                                   eta=                 0.3,
-                                   colsample_bytree=    1.0,
+                                   max_depth=           13,
+                                   min_child_weight=    6,
+                                   eta=                 0.0102,
+                                   colsample_bytree=    0.6376,
                                    gamma=               0.0,
                                    alpha=               0.0,
                                    lambda=              0.0,
@@ -53,8 +53,8 @@ entrega  <- as.data.table( list( "numero_de_cliente"= dataset[ foto_mes==202105,
                                  "Predicted"= as.integer( prediccion > 1/40 ) )  ) #genero la salida
 
 dir.create( "./exp/",  showWarnings = FALSE ) 
-dir.create( "./exp/KA7610/", showWarnings = FALSE )
-archivo_salida  <- "./exp/KA7610/KA7610_001.csv"
+dir.create( "./exp/KA7610_xgb/", showWarnings = FALSE )
+archivo_salida  <- "./exp/KA7610_xgb/KA7610_001.csv"
 
 #genero el archivo para Kaggle
 fwrite( entrega, 
