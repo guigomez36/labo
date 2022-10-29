2121ddab4615a1e71831c9c46df4e72564856c14
guigomez36 <guillermo.ggg.gomez@gmail.com>
2022-10-29 20:14:56 -0300

    experimento HT9422
     #Pleasi enter the commit message for your changes. Lines starting

--- src/competencia3/z942_HT_lightgbm_under.r
+++ src/competencia3/z942_HT_lightgbm_under.r
@@ -21,9 +21,9 @@ require("mlrMBO")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "HT9420"
+PARAM$experimento <- "HT9422"
 
-PARAM$exp_input  <- "TS9320"
+PARAM$exp_input  <- "TS9322"
 # FIN Parametros del script
 
 
