f4e04f66117d9b22afbd945a812701c6fd507574
guigomez36 <guillermo.ggg.gomez@gmail.com>
2022-10-30 11:27:26 -0300

    exp ZZ9424

--- src/competencia3/z992_ZZ_lightgbm_under.r
+++ src/competencia3/z992_ZZ_lightgbm_under.r
@@ -15,8 +15,8 @@ require("lightgbm")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "ZZ9423"
-PARAM$exp_input  <- "HT9423" 
+PARAM$experimento  <- "ZZ9424"
+PARAM$exp_input  <- "HT9424" 
 
 PARAM$modelos  <- 2
 # FIN Parametros del script
